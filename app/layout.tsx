b8e85ecd8e6816c97e6539fe1a28298a0cd29eba
azzamhaer <hudiya777@gmail.com>
2026-04-21 14:33:19 +0700

    feat: initialize global styles and root layout with LiveKit integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-
+import '@livekit/components-styles';
 const inter = Inter({
   subsets: ['latin'],
   variable: '--font-inter',
